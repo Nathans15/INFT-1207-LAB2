c82bf343d2da85677130f935b1473920e6861644
GeneralUseAcc <91762522+GeneralUseAcc@users.noreply.github.com>
2025-02-07 16:12:17 -0500

    Finished main

--- test_reading_list.py
+++ test_reading_list.py
@@ -1,16 +1,16 @@
 import unittest
-from reading_list import add_book, list_books, search_book
+from reading_list import add_book, delete_book, list_books, search_book
 
 
 class TestReadingList(unittest.TestCase):
     def test_add_book(self):
         add_book("Moby Dick", "Herman Melville", 1851)
         # Assert if the book is added (by manually checking CSV or creating validation logic)
-    def test_add_book(self):
+    def test_add_book_2(self):
         add_book("Animal Farm", "George Orwell", 1949)
 
-    def test_add_book(self):
-        add_book("Brave New World", "Aldous Huxley", 1932)
+    def test_delete_book(self):
+        delete_book("Brave New World", "Aldous Huxley", 1932)
 
     def test_search_book(self):
         search_book("Moby Dick")
